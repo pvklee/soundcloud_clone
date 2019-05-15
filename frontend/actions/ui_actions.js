c6e6be9579867ddf22c881250bced4137f40277c
Kevin Lee <pvklee@ucla.edu>
2019-05-14 23:12:44 -0700

    heroku fix ui reducer

---
+++ frontend/actions/ui_actions.js
@@ -0,0 +1,11 @@
+export const OPEN_SESSION_FORM_MODAL = "OPEN_SESSION_FORM_MODAL"
+export const CLOSE_SESSION_FORM_MODAL = "CLOSE_SESSION_FORM_MODAL"
+
+
+export const openSessionFormModal = () => ({
+  type: OPEN_SESSION_FORM_MODAL,
+})
+
+export const closeSessionFormModal = () => ({
+  type: CLOSE_SESSION_FORM_MODAL,
+})
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,6 +1,18 @@
-import {combineReducers} from 'redux'
+import {OPEN_SESSION_FORM_MODAL, CLOSE_SESSION_FORM_MODAL} from '../actions/ui_actions'
+import merge from 'lodash/merge'
 
+const initialState = {
+  modalState: 'NONE'
+}
 
-export default combineReducers({
-
-})
+export default (state = initialState, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case OPEN_SESSION_FORM_MODAL:
+      return merge({}, state, {modalState: 'SESSION_FORM'})
+    case CLOSE_SESSION_FORM_MODAL:
+      return merge({}, state, {modalState: 'NONE'})
+    default:
+      return state;
+  }
+}
